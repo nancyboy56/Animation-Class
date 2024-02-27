18ed03545db8dc9a5178aca3f78ff86e021a9eab
Casey <gooby5656@gmail.com>
2024-02-27 19:38:01 +1100

    basic level no movement

---
+++ Sidescroller tests/Prototype Animation/Prototype Animation/Assets/Scripts/Controls/BasicMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasicMovement : MonoBehaviour
+{
+    [SerializeField]
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("Start Movement!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
