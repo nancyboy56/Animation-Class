138e4631c96e6fd23e862a23c26ed457d628edd3
Casey <gooby5656@gmail.com>
2024-02-27 23:07:30 +1100

    added a basic noise shader

--- Sidescroller tests/Prototype Animation/Prototype Animation/Assets/Scripts/Controls/BasicMovement.cs
+++ Sidescroller tests/Prototype Animation/Prototype Animation/Assets/Scripts/Controls/BasicMovement.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class BasicMovement : MonoBehaviour
 {
-  
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float moveSpeed = 1;
     
     // Start is called before the first frame update
     void Start()
     {
         //print("Start Movement!");
+        rb =GetComponent<Rigidbody2D>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        print("Move");
+        rb.velocity= context.ReadValue<Vector2>() * moveSpeed;
+
+        //rb.velocity = moveSpeed;
     }
 }
