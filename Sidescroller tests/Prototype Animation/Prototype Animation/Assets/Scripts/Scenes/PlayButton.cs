bed4034d9d25bdc73e1d6cd11f066c9470eb3457
Casey <gooby5656@gmail.com>
2024-05-05 21:02:31 +1000

    play button works

---
+++ Sidescroller tests/Prototype Animation/Prototype Animation/Assets/Scripts/Scenes/PlayButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    [SerializeField] private string nextScene = "Loading";
+    [SerializeField] private string folder = "v0.3";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Scenes/"+ folder + "/" + nextScene, LoadSceneMode.Single);
+    }
+}
